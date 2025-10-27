1b711dc1e3ac411f66217c0a3f482bd8731b4048
jisunyoon <yjsun0123@daum.net>
2025-10-27 23:31:42 +0900

    feat: 색상 정의 및 main 작업 중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import logo from "@/assets/logo.svg";
 import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Start</Button>
+    <div className="py-5 px-6">
+      <div className="flex justify-center items-center">
+        <h1><img src={logo} alt="logo" className="w-25 h-25" /></h1>
+      </div>
+      <Button className="w-full text-xl font-semibold h-10 bg-point text-primary">Start</Button>
     </div>
   );
 }
