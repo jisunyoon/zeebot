cae256122a3c430b81d7ab313e5b5bf9cf420a9c
jisunyoon <yjsun0123@daum.net>
2025-11-03 23:03:40 +0900

    feat: home layout 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,15 @@ import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
-    <div className="py-5 px-6">
-      <div className="flex justify-center items-center">
-        <h1><img src={logo} alt="logo" className="w-25 h-25" /></h1>
+    <div className="h-full flex flex-col">
+      <div className="flex-1 flex flex-col items-center justify-center gap-8">
+        <h1><img src={logo} alt="logo" className="w-25 h-25" /><span className="hidden">Logo</span></h1>
+        <div className="flex flex-col text-center">
+          <p className="text-lg text-black"><strong>ZeeBot</strong>에게 물어보세요!</p>
+          <span className="text-xs text-gray-400">궁금한 점이 있나요?<br />ZeeBot이 바로 알려드릴게요.</span>
+        </div>
       </div>
-      <Button className="w-full text-xl font-semibold h-10 bg-point text-primary">Start</Button>
+      <Button className="w-full text-xl font-semibold h-10 bg-point text-primary cursor-pointer hover:bg-point/80">Start</Button>
     </div>
   );
 }
